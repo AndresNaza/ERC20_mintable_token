817dba828b882f058b4e7823a7bc09424458f7cb
Andres <andres.nazarian@hotmail.com>
2021-09-28 16:44:59 -0300

    Deploy on Ropsten testnet

---
+++ README.md
@@ -0,0 +1,25 @@
+# StarDucks Cappucino - ERC20 mintable token sale 
+
+The repo contains a **Solidity/Truffle** project that creates and deploys a ERC20 mintable token sale, so that there is **no upper limit to the Total Supply**.
+
+The token sale contract has been deployed on Ropsten testnet, under the address 0x3F4DAd70aB66343E27998817ce4B7e117651d20d. On this subject, you can also check and experiment on that testnet with the react front end available in [here](https://andresnaza.github.io/ERC20_mintable_token/). As you can see, the DApp contains a simple whitelist, or KYC functionality, developed considering that those requirements can be mandatory for many different applications nowadays.
+
+Although this project has been created as a work sample for my project portfolio, the work here can be use in real case scenarios, such as:
+ 
+## Real-World Use-Case for this Project
+
+💰 Tokenization of any Assets as fungible Tokens (ERC20)
+
+🏦 Creation of Bonus Programs, Vouchers, etc.
+
+💲 Creation of a new crypto currency
+
+🧾 Creation of a Payment-layer on top of Ethereum
+
+## Development-Goal
+
+🤖 Understand deployment of dApps
+
+🦸‍♂️ Understand Tokenization using Open-Zeppelin Smart Contracts
+
+☑️ Deeper dive into Unit-Testing
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 require("dotenv").config({path: "./.env"});
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const AccountIndex = 0;
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -18,21 +18,21 @@ module.exports = {
     }, */
     ganache_local: {
       provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex)
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex)
       },
       network_id: 5777
     },
-    goerli_infura: {
-      provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/ffa60321cbbd4cfda3352014c556e3c3", AccountIndex)
-      },
-      network_id: 5
-    },
     ropsten_infura: {
       provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/ffa60321cbbd4cfda3352014c556e3c3", AccountIndex)
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.YOUR_INFURA_ID, MetaMaskAccountIndex)
       },
       network_id: 3
+    },
+    goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/" + process.env.YOUR_INFURA_ID, MetaMaskAccountIndex)
+      },
+      network_id: 5
     }
   },
   compilers: {
