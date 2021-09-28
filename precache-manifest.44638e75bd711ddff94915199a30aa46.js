019597bfe492aae725da51602f0610f3ef42539d
Andres <andres.nazarian@hotmail.com>
2021-09-28 16:50:00 -0300

    Updates

---
+++ precache-manifest.44638e75bd711ddff94915199a30aa46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e4ef1e6105de1d1e56afad0436a31d",
+    "url": "/ERC20_mintable_token/index.html"
+  },
+  {
+    "revision": "b51c70a48329b43d9385",
+    "url": "/ERC20_mintable_token/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bfd995a89730974dae73",
+    "url": "/ERC20_mintable_token/static/js/2.995d5c7a.chunk.js"
+  },
+  {
+    "revision": "b51c70a48329b43d9385",
+    "url": "/ERC20_mintable_token/static/js/main.2d8fde2c.chunk.js"
+  },
+  {
+    "revision": "a96f4a1e65929df870b1",
+    "url": "/ERC20_mintable_token/static/js/runtime-main.ef587883.js"
+  }
+]);
